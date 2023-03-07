aad8170d16e22153e410a66651cb9a277b255e40
Tracy L. Kimbrough <Ladyjettt@gmail.com>
2023-03-07 15:12:25 -0600

    2nd commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import Header from "./header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+
+
+function App() {
+
+    const contacts = [
+        {
+            id:"1",
+            name:"Tracy",
+            email: "ladyjettt@gmail.com",
+        },
+        {
+            id:"2",
+            name:"James",
+            email: "luxxbundles@gmail.com",
+        },
+    ];
+    return( 
+       <div className ="ui container">
+            <Header />
+            <AddContact />
+            <ContactList contacts={contacts}/>
+     </div>                      
+    );
+}
+export default App;
